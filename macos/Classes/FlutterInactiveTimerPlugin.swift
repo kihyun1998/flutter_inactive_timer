c50167db994b76069f015c5dda467c0eae371be8
Ki Hyun Park <kihyun1998@penta.co.kr>
2025-04-20 21:40:35 +0900

    FEAT: macos supported

--- macos/Classes/FlutterInactiveTimerPlugin.swift
+++ macos/Classes/FlutterInactiveTimerPlugin.swift
@@ -1,5 +1,7 @@
 import Cocoa
 import FlutterMacOS
+import IOKit
+import IOKit.hid
 
 public class FlutterInactiveTimerPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -10,10 +12,49 @@ public class FlutterInactiveTimerPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "getPlatformVersion":
-      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
+    case "getSystemTickCount":
+      result(getSystemUptimeInMilliseconds())
+    case "getLastInputTime":
+      if let lastInput = getLastInputTime(){
+        result(lastInput)
+      }else {
+        result(FlutterError(code: "UNAVAILABLE", message: "Cannot get last input time", details: nil))
+      }
+
     default:
       result(FlutterMethodNotImplemented)
     }
+  }
+
+  private func getSystemUptimeInMilliseconds() -> UInt64{
+    let uptime = ProcessInfo.processInfo.systemUptime
+    return UInt64(uptime * 1000)
+  }
+
+  private func getLastInputTime() -> UInt64? {
+    var iterator = io_iterator_t()
+    let matchingDict = IOServiceMatching("IOHIDSystem")
+
+    guard IOServiceGetMatchingServices(kIOMasterPortDefault, matchingDict, &iterator) == KERN_SUCCESS else {
+      return nil
+    }
+
+    let entry =  IOIteratorNext(iterator)
+    IOObjectRelease(iterator)
+    guard entry != 0 else { return nil }
+
+    var properties: Unmanaged<CFMutableDictionary>?
+    guard IORegistryEntryCreateCFProperties(entry, &properties, kCFAllocatorDefault, 0) == KERN_SUCCESS,
+          let dict = properties?.takeRetainedValue() as? [String: AnyObject],
+          let hidIdleTime = dict["HIDIdleTime"] as? UInt64 else {
+      IOObjectRelease(entry)
+      return nil
+    }
+
+    IOObjectRelease(entry)
+
+    let idleMillis = hidIdleTime / 1_000_000
+    return getSystemUptimeInMilliseconds() - idleMillis
+
   }
 }
